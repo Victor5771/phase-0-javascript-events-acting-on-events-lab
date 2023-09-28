308ca975e532e38976f2d0c71082c80f8bc9db73
Victor <victor.kyalo@student.moringaschool.com>
2023-09-28 12:11:46 +0300

    complete

--- prac.js
+++ prac.js
@@ -1,4 +1,3 @@
-// Your code here
 // Define the Dodger element
 const dodger = document.getElementById('dodger');
 
@@ -18,14 +17,14 @@ document.addEventListener('keydown', function (e) {
 
 // Define the moveDodgerLeft() function
 function moveDodgerLeft() {
-  let left = parseInt(dodger.style.left) || 0;
-  const newLeft = Math.max(left - 1, 0); 
-  dodger.style.left = `${newLeft}px`; 
+  let left = parseInt(dodger.style.left) || 0; // Get the current left position or default to 0
+  const newLeft = Math.max(left - 1, 0); // Calculate the new left position and ensure it's not negative
+  dodger.style.left = `${newLeft}px`; // Update the Dodger's position
 }
 
 // Define the moveDodgerRight() function
 function moveDodgerRight() {
-  let left = parseInt(dodger.style.left) || 0; 
-  const newLeft = Math.min(left + 1, 360);
-  dodger.style.left = `${newLeft}px`; 
+  let left = parseInt(dodger.style.left) || 0; // Get the current left position or default to 0
+  const newLeft = Math.min(left + 1, 360); // Calculate the new left position and ensure it's within the game field width (400 - Dodger width)
+  dodger.style.left = `${newLeft}px`; // Update the Dodger's position
 }
